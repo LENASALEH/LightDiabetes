0c506197e15e6cd4714079038cc261e45fe363e6
saralmuhanna <saralmuhannaa@gmail.com>
2022-04-18 22:48:08 +0300

    SARA

--- app/src/main/java/com/example/lightdiabetes/Activity2.java
+++ app/src/main/java/com/example/lightdiabetes/Activity2.java
@@ -1,14 +1,36 @@
 package com.example.lightdiabetes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity2 extends AppCompatActivity {
-
+    Button btn;
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        btn=findViewById(R.id.AddMeals);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), Activity3.class);
+                startActivity(intent);
+            }
+        } );
+
+        btn2=findViewById(R.id.InsulinDoses);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), Activity6.class);
+                startActivity(intent);
+            }
+        } );
     }
 }
--- app/src/main/java/com/example/lightdiabetes/Activity6.java
+++ app/src/main/java/com/example/lightdiabetes/Activity6.java
@@ -1,29 +1,26 @@
 package com.example.lightdiabetes;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
-public class Activity3 extends AppCompatActivity {
+public class Activity6 extends AppCompatActivity {
     Button btn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_3);
-
-        btn=findViewById(R.id.Next);
+        setContentView(R.layout.activity_6);
+        btn=findViewById(R.id.MeasureNextDose);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent( getBaseContext(),Activity5.class );
+                Intent intent = new Intent( getBaseContext(),Activity7.class );
                 startActivity(intent);
             }
         });
+
     }
 }
